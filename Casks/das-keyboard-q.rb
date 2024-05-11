29b51ced885c6b49190a1117bb3c3852a7a8c193
William Artero <git@artero.dev>
2024-05-11 09:55:31 +0200

    chore: updated das-keyboard-q to v4
    Now they use dmg instead of pkg

--- Casks/das-keyboard-q.rb
+++ Casks/das-keyboard-q.rb
@@ -1,20 +1,24 @@
 cask "das-keyboard-q" do
-  version "3.3.3"
-  sha256 "bed11ee909bf1b075c85a7212770bf3246a549b8ec38f248c1bafb3e56590559"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://download.daskeyboard.com/q-software-releases/#{version}/Das-Keyboard-Q_#{version}.pkg", verified: "download.daskeyboard.com/q-software-releases/"
+  version "4.1.3"
+  sha256 arm:   "22e2004730467aa36c1ceca35aaf44e31d2b81bfa4fa838e4026c7d30df4f7b8",
+         intel: "768c4428edc983fae33bba35ddd7a1217caba3149ddfa68b3a35634ed3a7f6ec"
+
+  url "https://das-keyboard-q-releases.s3.us-east-2.amazonaws.com/das-keyboard-q/darwin/#{arch}/Das+Keyboard+Q-#{version}-#{arch}.dmg", verified: "das-keyboard-q-releases.s3.us-east-2.amazonaws.com/das-keyboard-q/darwin/"
   name "Das Keyboard Q"
   desc "Display Information on Das Keyboard Q RGB Devices"
   homepage "https://www.daskeyboard.io"
 
   livecheck do
     url "https://www.daskeyboard.io/get-started/software/"
-    regex(/href=.*?Das-Keyboard-Q[_-]v?(\d+(?:\.\d+)+)\.pkg/i)
+    regex(/href=.*?Das+Keyboard+Q-?(\d+(?:\.\d+)+)-#{arch}\.dmg/i)
   end
 
-  pkg "Das-Keyboard-Q_#{version}.pkg"
+  app "Das Keyboard Q.app"
 
-  uninstall pkgutil: ["com.daskeyboard.q"]
+  uninstall quit:    "com.daskeyboard.q",
+            pkgutil: "com.daskeyboard.q"
 
   zap trash: [
     "~/.quio",
