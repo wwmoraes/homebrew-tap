c72e57fd1ed0386b97fe36cbe778e6f0e3e3162f
William Artero <git@artero.dev>
2024-09-26 19:13:42 +0200

    chore: updated displaylink-manager to 1.10.3

--- Casks/displaylink-manager.rb
+++ Casks/displaylink-manager.rb
@@ -1,8 +1,8 @@
 cask "displaylink-manager" do
-  version "1.8"
-  sha256 "8cd7e7aeb393a707019a7222ce73f2abf2f662e8d1c00f5176b1f3b32ac7fb13"
+  version "1.10.3"
+  sha256 "f4e8c36b6278db22bbf2c286490acd5c2d323b07e4d4e9870c6e0ff9017372d9"
 
-  url "https://www.synaptics.com/sites/default/files/exe_files/2022-10/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
+  url "https://www.synaptics.com/sites/default/files/exe_files/2024-08/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
   name "DisplayLink Manager"
   desc "Enables DisplayLink docks, adapters and monitors"
   homepage "https://www.synaptics.com/products/displaylink-graphics"
